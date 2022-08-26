ce9f4841c99cb4afa95a6ff3d78bd5d5835fdbbc
john-waczak <john.louis.waczak@gmail.com>
2022-08-25 23:04:27 -0500

    fixing link and changing capitalization

--- src/SolarGeometry.jl
+++ src/SolarGeometry.jl
@@ -9,14 +9,14 @@ export solar_azimuth_altitude
 """
     function solar_azimuth_altitude(utc_time, lat, lon, alt)
 
-Given a UTC time `utc_time`, latitude `Lat`, longitude `Lon`, and altitutde `alt`, return the solar azimuth and solar elevation angles (in degrees) relative to that location.
+Given a UTC time `utc_time`, latitude `lat`, longitude `lon`, and altitutde `alt`, return the solar azimuth and solar elevation angles (in degrees) relative to that location.
 
 
 This is a Julia (re)implementation of the Matlab script by Darin C. Koblick [(link)](https://www.mathworks.com/matlabcentral/fileexchange/23051-vectorized-solar-azimuth-and-elevation-estimation).
 
 Other references:
-- http://stjarnhimlen.se/comp/tutorial.html#5
-- http://www.stargazing.net/kepler/altaz.html
+- [](http://stjarnhimlen.se/comp/tutorial.html#5)
+- [](http://www.stargazing.net/kepler/altaz.html)
 """
 function solar_azimuth_altitude(UTC::DateTime, Lat, Lon, Alt)
     jd = datetime2julian(UTC)

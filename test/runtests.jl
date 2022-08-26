f460bb95c09fd03f4f38b33526b408289ff84dc7
john-waczak <john.louis.waczak@gmail.com>
2022-08-25 22:43:25 -0500

    fixed function name typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
 @testset "SolarGeometry.jl" begin
     # Write your tests here.
     dt = DateTime(1990, 4, 19, 0, 0, 0)
-    az, alt = solar_azimuth_elevation(dt, 60.0, 15.0, 0)
+    az, alt = solar_azimuth_altitude(dt, 60.0, 15.0, 0)
     @test isapprox(az, 15.68, atol=0.01)
     @test isapprox(alt, -17.96, atol=0.01)
 end

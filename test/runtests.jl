ed51cd6aa62e6edd51929e7c5454601f642d9cfe
John Waczak <john.louis.waczak@gmail.com>
2023-03-08 14:06:19 -0600

    adding second version of function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using SolarGeometry
-using Dates: DateTime
+using Dates: DateTime, second
 using Test
 
 @testset "SolarGeometry.jl" begin
@@ -8,6 +8,17 @@ using Test
     az, alt = solar_azimuth_altitude(dt, 60.0, 15.0, 0)
     @test isapprox(az, 15.68, atol=0.01)
     @test isapprox(alt, -17.96, atol=0.01)
+
+
+    # test out second version
+    Δt = 1.0
+    dt2 = DateTime(1990, 4, 19, 0, 0, 1)
+    println(dt)
+    println(dt2)
+    az, alt = solar_azimuth_altitude(Δt, dt, 60.0, 15.0, 0)
+    az2, alt2 = solar_azimuth_altitude(dt2, 60.0, 15.0, 0)
+    @test isapprox(az,az2, rtol=1e-3)
+    @test isapprox(alt,alt2, rtol=1e-3)
 end
 
 
